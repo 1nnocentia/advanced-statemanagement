252fa17ac972cfbd5ceef9595029ab5a0af97143
inno <innocentiahan14@gmail.com>
2025-09-25 08:59:45 +0800

    pengumpulan

---
+++ local_state_management/local_state_management/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart'; 
+
+void main() => runApp(MyEphemeralApp()); 
+
+class MyEphemeralApp extends StatelessWidget { 
+  @override Widget build(BuildContext context) {
+    return MaterialApp( 
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ephemeral State Example')), 
+          body: CounterWidget(), ), );
+  } 
+} 
+    
+class CounterWidget extends StatefulWidget { 
+  @override _CounterWidgetState createState() => _CounterWidgetState();
+} 
+
+class _CounterWidgetState extends State<CounterWidget> { 
+  int _counter = 0; 
+  
+  @override Widget build(BuildContext context) { 
+    return Center( 
+      child: Column( 
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: <Widget>[ 
+          Text('Counter Value: $_counter'), 
+          SizedBox(height: 10), 
+          ElevatedButton( onPressed: () { 
+            setState(() { 
+              _counter++; 
+            }); 
+          }, 
+          child: Text('Increment'), 
+          ),
+          SizedBox(height: 12),
+          ElevatedButton( onPressed: () { 
+            setState(() { 
+              if (_counter > 0) {
+                _counter--; 
+              }
+            }); 
+          }, 
+          child: Text('Decrement'), 
+          ),  
+        ], 
+      ), 
+    ); 
+  } 
+}
